82893d49ed80c2967c9146c6233e1c0d139f8a37
Mohomad Ashfaq <ashfaqmohamed411@gmail.com>
2024-11-11 18:18:45 +0530

    auto scroll banner

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ function gotoDonation(){
     const btn = document.getElementById('donation-body');
     btn.scrollIntoView({ behavior:'smooth' });
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    let slideIndex = 0;
+    showSlides();
+  
+    function showSlides() {
+      let slides = document.getElementsByClassName("mySlides");
+      for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; // Hide all slides
+      }
+      slideIndex++;
+      if (slideIndex > slides.length) { slideIndex = 1 } // Reset to first slide after the last one
+      slides[slideIndex - 1].style.display = "block"; // Show the current slide
+      setTimeout(showSlides, 2000); // Change image every 2 seconds
+    }
+  });
+  
