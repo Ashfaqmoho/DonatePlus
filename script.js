a0646e7f2b58b33506755410c4779b9001e77b5c
Mohomad Ashfaq <ashfaqmohamed411@gmail.com>
2024-11-15 01:00:26 +0530

    new donation page

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
-function gotoDonation(){
-    const btn = document.getElementById('donation-body');
-    btn.scrollIntoView({ behavior:'smooth' });
+function gotoDonation() {
+  const btn = document.getElementById('donation-body');
+  btn.scrollIntoView({ behavior: 'smooth' });
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    let slideIndex = 0;
-    showSlides();
-  
-    function showSlides() {
-      let slides = document.getElementsByClassName("mySlides");
-      for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none"; // Hide all slides
-      }
-      slideIndex++;
-      if (slideIndex > slides.length) { slideIndex = 1 } // Reset to first slide after the last one
-      slides[slideIndex - 1].style.display = "block"; // Show the current slide
-      setTimeout(showSlides, 2000); // Change image every 2 seconds
+function gotoAbout() {
+  const btn = document.getElementById('about');
+  btn.scrollIntoView({ behavior: 'smooth' });
+}
+
+function gotoChatBot() {
+  const btn = document.getElementById('help-container');
+  btn.scrollIntoView({ behavior: 'smooth' });
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  let slideIndex = 0;
+  showSlides();
+
+  function showSlides() {
+    let slides = document.getElementsByClassName("mySlides");
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; // Hide all slides
     }
-  });
-  
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 } // Reset to first slide after the last one
+    slides[slideIndex - 1].style.display = "block"; // Show the current slide
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+  }
+});
+
+
+
